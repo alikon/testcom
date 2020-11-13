cc5135225216ecdd85b4044d87a599203beaf140
alikon <optimus4joomla@gmail.com>
2020-11-13 19:58:26 +0100

    clean

--- com_jobs/helpers/jobs.php
+++ com_jobs/helpers/jobs.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_redirect
+ * @subpackage  com_jobs
  *
  * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -12,8 +12,7 @@ defined('_JEXEC') or die;
 /**
  * Redirect component helper.
  *
- * @since       1.6
- * @deprecated  5.0 Use \Joomla\Component\Redirect\Administrator\Helper\RedirectHelp instead
+ * @since       __DEPLOY_VERSION__
  */
 class JobsHelper extends \Joomla\Component\Jobs\Administrator\Helper\JobsHelper
 {
