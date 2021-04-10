db128b70f80e39a987ea4ccaf546984d192fbd63
Nicola Galgano <optimus4joomla@gmail.com>
2021-04-10 10:49:44 +0200

    console plugin

---
+++ src/plugins/console/job/services/provider.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package   job
+ * @copyright Copyright (c)2021 Alikon
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die;
+
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Plugin\Console\Job\Extension\Job;
+
+return new class implements ServiceProviderInterface
+{
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function register(Container $container)
+	{
+		$container->set(
+			PluginInterface::class,
+			function (Container $container)
+			{
+				// @Todo This needs to be changed
+				$plugin = \Joomla\CMS\Plugin\PluginHelper::getPlugin('console', 'job');
+
+				return new Job(
+					$container->get(DispatcherInterface::class),
+					(array) $plugin
+				);
+			}
+		);
+	}
+};
