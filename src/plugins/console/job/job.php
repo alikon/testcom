9bf00905ebbcc7861ac9715babc35c252abb7e29
Nicola Galgano <optimus4joomla@gmail.com>
2021-04-14 08:25:37 +0200

    refactoring

---
+++ src/plugins/console/job/job.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package   Job
+ * @copyright Copyright (c)2021 Alikon
+ * @license   GNU General Public License version 3, or later
+ */
+
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+class plgConsoleJob extends CMSPlugin
+{
+}
