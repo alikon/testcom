3963ae11fec3fed2a2dc17bc62457e3b9c1a3436
Nicola Galgano <optimus4joomla@gmail.com>
2021-07-04 09:08:31 +0200

    duration

--- com_jobs/admin/install/sql/mysql/install.sql
+++ com_jobs/admin/install/sql/mysql/install.sql
@@ -2,14 +2,14 @@ CREATE TABLE IF NOT EXISTS `#__tasks`
 (
     `id`       int(11)  NOT NULL AUTO_INCREMENT,
     `taskname` varchar(100) NOT NULL,
-    `duration` int(11)     NOT NULL,
+    `duration` DECIMAL(5,3) NOT NULL,
     `jobid`    int(11)     NOT NULL,
     `taskid`   int(11)     NOT NULL,
     `exitcode` int(11)     NOT NULL,
     `lastdate` datetime,
     `nextdate` datetime,
-
-    
+    `frequency` int unsigned NOT NULL DEFAULT '0',
+    `unit` varchar(100),
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
