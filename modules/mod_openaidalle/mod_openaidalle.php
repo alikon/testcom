3b7846dad8e95606fa991b819c9a79dbbc0186cb
Nicola Galgano <optimus4joomla@gmail.com>
2023-01-04 09:57:23 +0100

    dall-e.0.1

---
+++ modules/mod_openaidalle/mod_openaidalle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_openaidalle
+ *
+ * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+require ModuleHelper::getLayoutPath('mod_openaidalle', $params->get('layout', 'default'));
