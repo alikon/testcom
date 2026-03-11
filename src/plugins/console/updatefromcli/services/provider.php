582bfdb11e3bfbea2bfd7abb1e2c50075f6b4eb1
Nicola Galgano <optimus4joomla@gmail.com>
2026-03-11 10:49:49 +0100

    license

--- src/plugins/console/updatefromcli/services/provider.php
+++ src/plugins/console/updatefromcli/services/provider.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Console.updatefromcli
+ *
+ * @copyright   Copyright (C) 2026 Alikon. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\PluginInterface;
--- src/plugins/console/updatefromcli/src/CliCommand/UpdatefromcliCommand.php
+++ src/plugins/console/updatefromcli/src/CliCommand/UpdatefromcliCommand.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Console.updatefromcli
+ *
+ * @copyright   Copyright (C) 2026 Alikon. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 namespace Joomla\Plugin\Console\Updatefromcli\CliCommand;
 
 use Joomla\CMS\Factory;
