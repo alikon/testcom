91911d1df6cc8cf0867edba3fd1a62d48f1e09ef
alikon <optimus4joomla@gmail.com>
2026-03-18 12:18:57 +0100

    addt test folder

---
+++ tests/cypress/support/index.js
@@ -0,0 +1,15 @@
+import('./commands.mjs');
+import('joomla-cypress');
+
+before(() => {
+  cy.task('startMailServer');
+  cy.task('clearLogs');
+});
+
+afterEach(() => {
+  cy.checkForPhpNoticesOrWarnings();
+  // Check for logs
+  cy.task('checkForLogs');
+  cy.task('cleanupDB');
+  Cypress.session.clearAllSavedSessions();
+});
