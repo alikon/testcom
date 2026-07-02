1c0fa2e4f9ee61d3231cd6070b604ce4d76a10db
Nicola Galgano <optimus4joomla@gmail.com>
2026-07-02 10:51:30 +0200

    Refactor contact form submission into a function

--- tests/cypress/integration/plugins/ContactCustomReply.cy.js
+++ tests/cypress/integration/plugins/ContactCustomReply.cy.js
@@ -1,5 +1,18 @@
+const submitContactForm = () => {
+  cy.get('#jform_contact_name').type('Test User');
+  cy.get('#jform_contact_email').type('testuser@example.com');
+  cy.get('#jform_contact_emailmsg').type('Test Subject');
+  cy.get('#jform_contact_message').type('Test message content');
+  cy.get('button.btn.btn-primary.validate[type="submit"]').click();
+  //cy.get('.alert-success').should('be.visible');
+};
+
 describe('Test in frontend that the contact form view', () => {
-  afterEach(() => cy.task('queryDB', 'DELETE FROM #__contact_details'));
+  afterEach(() => {
+    cy.task('queryDB', 'DELETE FROM #__contact_details');
+    cy.db_updateExtensionParameter('custom_reply', '0', 'com_contact');
+    cy.db_enableExtension('0', 'plg_contact_customreply');
+  });
 
   it('can create a contact through a form', () => {
     cy.doFrontendLogin();
@@ -19,15 +32,16 @@ describe('Test in frontend that the contact form view', () => {
     cy.db_getUserId().then((id) => cy.db_createContact({ name: 'test contact', user_id: id }))
       .then((contact) => {
         cy.visit(`/index.php?option=com_contact&view=contact&id=${contact.id}`);
-        cy.get('#jform_contact_name').type('Test User');
-        cy.get('#jform_contact_email').type('testuser@example.com');
-        cy.get('#jform_contact_emailmsg').type('Test Subject');
-        cy.get('#jform_contact_message').type('Test message content');
-        cy.get('button.btn.btn-primary.validate[type="submit"]').click();
+        submitContactForm();
 
         cy.task('getMails').then((mails) => {
-          expect(mails.length).to.be.greaterThan(0);
-          cy.wrap(mails[0].body).should('contain', 'Test message content');
+          expect(mails).to.have.length(1);
+          const mail = mails[0];
+          if (contact.email_to) {
+            expect(mail.to).to.contain(contact.email_to);
+          }
+          expect(mail.subject).to.contain('Test Subject');
+          expect(mail.body).to.contain('Test message content');
         });
       });
   });
@@ -39,15 +53,14 @@ describe('Test in frontend that the contact form view', () => {
     cy.db_getUserId().then((id) => cy.db_createContact({ name: 'test contact', user_id: id }))
       .then((contact) => {
         cy.visit(`/index.php?option=com_contact&view=contact&id=${contact.id}`);
-        cy.get('#jform_contact_name').type('Test User');
-        cy.get('#jform_contact_email').type('testuser@example.com');
-        cy.get('#jform_contact_emailmsg').type('Test Subject');
-        cy.get('#jform_contact_message').type('Test message content');
-        cy.get('button.btn.btn-primary.validate[type="submit"]').click();
+        submitContactForm();
 
         cy.task('getMails').then((mails) => {
-          expect(mails.length).to.be.greaterThan(0);
-          cy.wrap(mails[0].body).should('contain', 'Test message content');
+          expect(mails).to.have.length(2);
+          const toAddresses = mails.map((m) => m.to).join(' ');
+          expect(toAddresses).to.contain('testuser@example.com');
+          mails.forEach((mail) => expect(mail.subject).to.contain('Test Subject'));
+          mails.forEach((mail) => expect(mail.body).to.contain('Test message content'));
         });
       });
   });
